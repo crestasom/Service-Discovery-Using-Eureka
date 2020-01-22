bb904c2b191f755a0cbedc1adf475b514f2215cf
crestasom <crestasom@gmail.com>
2020-01-22 10:51:01 +0545

    init commit

---
+++ src/test/java/com/crestasom/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crestasom.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
